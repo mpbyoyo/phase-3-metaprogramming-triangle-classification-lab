f6eccb54de83cf3ec9f811bf6f8631e3310d794e
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-06-07 17:37:34 -0500

    commit tree e6c9f6e8fba4375e974c4fbddcbbf9fb6efd7a29

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  def initialize(len1, len2, len3)
+    @len1 = len1
+    @len2 = len2 
+    @len3 = len3 
+  end
+  attr_reader :len1, :len2, :len3
+
+  def kind
+    len_arr = [len1, len2, len3]
+    if len_arr.find {|i| i <= 0} || len_arr.sort[0] + len_arr.sort[1] <= len_arr.sort[2]
+      begin
+        raise TriangleError
+      end
+    else
+      case len_arr.uniq.length
+      when 1 then :equilateral
+      when 2 then :isosceles
+      when 3 then :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle"
+    end
+  end
 end
